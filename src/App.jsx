7660e5754b8d124f0a448a8a1aa0bc6277aca940
unknown <seangsang>
2024-12-18 22:25:30 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import Route from "./Routes/MenuBar"
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import { Home, About, Contact, Layout, ErrorPage } from "../page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "About",
+        element: <About />,
+      },
+      {
+        path: "Contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <div><Route/></div>
-  )
-}
+  return <RouterProvider router={router} />
+};
 
-export default App
+export default App;
